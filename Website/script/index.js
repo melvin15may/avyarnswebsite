4e2534b466f79c9fc8fc229376b51dd88994fd89
melvin15may <melvin15may@gmail.com>
2013-11-14 21:20:21 +0530

    Added new files (jpgs nd index.js) carousel added
    changes to index.html and website.css

---
+++ Website/script/index.js
@@ -0,0 +1,16 @@
+//get a reference to the canvas
+$(document).ready(function() {
+	init();
+	initParticleSystem();
+});
+
+
+//casousel 
+
+(function($) {
+    'use strict';
+
+    $(function() {
+        $('#myCarousel').carousel();
+    });
+})(window.jQuery);
