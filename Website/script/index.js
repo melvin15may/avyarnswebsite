faffded9f6e31bc7c362b122094383057f367ed5
Melvin <melvin@ubuntu.ubuntu-domain>
2014-11-11 23:28:12 +0530

    about us done with parallax

--- Website/script/index.js
+++ Website/script/index.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 });
 
 
-//casousel 
+//casousel
 
 (function($) {
     'use strict';
@@ -27,3 +27,27 @@ $(function(){
         $(this).removeClass('open');
     });
 });
+
+//parallax
+
+$(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+
+   $('section[data-type="background"]').each(function(){
+     // declare the variable to affect the defined data-type
+     var $scroll = $(this);
+
+      $(window).scroll(function() {
+        // HTML5 proves useful for helping with creating JS functions!
+        // also, negative value because we're scrolling upwards
+        var yPos = -($window.scrollTop() / $scroll.data('speed'));
+
+        // background position
+        var coords = '50% '+ yPos + 'px';
+
+        // move the background
+        $scroll.css({ backgroundPosition: coords });
+      }); // end window scroll
+   });  // end section function
+}); // close out script
