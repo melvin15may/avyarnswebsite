e305250c88fb19efde6afe084a0616e32a9fbf30
Melvin <melvin@ubuntu.ubuntu-domain>
2014-11-13 00:42:36 +0530

    client added

--- Website/script/index.js
+++ Website/script/index.js
@@ -51,3 +51,67 @@ $(document).ready(function(){
       }); // end window scroll
    });  // end section function
 }); // close out script
+
+//business section navigation
+
+$('.container-fluid > #business > .container-fluid > row > .polyster').hover(function() {
+        $(this).find('.container-fluid > #business > .container-fluid > row > .polyster > .polyster-head').hide();
+        $(this).find('.container-fluid > #business > .container-fluid > row > .polyster > .polyster-div').show();
+    }, function() {
+        $(this).find('.container-fluid > #business > .container-fluid > row > .polyster > .polyster-div').hide();
+        $(this).find('.container-fluid > #business > .container-fluid > row > .polyster > .polyster-head').show();
+});
+
+if (window.console) console.log('foo');
+
+$('.switch').hover(function() {
+    if (window.console) console.log('HOvering');
+        $(this).find('.avg_words').hide();
+        $(this).find('.avg_num').show();
+    }, function() {
+        if (window.console) console.log('HOvering');
+        $(this).find('.avg_num').hide();
+        $(this).find('.avg_words').show();
+});
+
+function polyster_mousein(){
+    var x = document.getElementsByClassName("polyster-div");
+    x[0].style.display = "block";
+    x = document.getElementsByClassName("polyster-head");
+    x[0].style.display = "none";
+}
+
+function cotton_mousein(){
+    var x = document.getElementsByClassName("cotton-div");
+    x[0].style.display = "block";
+    x = document.getElementsByClassName("cotton-head");
+    x[0].style.display = "none";
+}
+
+function acrylic_mousein(){
+    var x = document.getElementsByClassName("acrylic-div");
+    x[0].style.display = "block";
+    x = document.getElementsByClassName("acrylic-head");
+    x[0].style.display = "none";
+}
+
+function polyster_mouseout(){
+    var x = document.getElementsByClassName("polyster-div");
+    x[0].style.display = "none";
+    x = document.getElementsByClassName("polyster-head");
+    x[0].style.display = "block";
+}
+
+function cotton_mouseout(){
+    var x = document.getElementsByClassName("cotton-div");
+    x[0].style.display = "none";
+    x = document.getElementsByClassName("cotton-head");
+    x[0].style.display = "block";
+}
+
+function acrylic_mouseout(){
+    var x = document.getElementsByClassName("acrylic-div");
+    x[0].style.display = "none";
+    x = document.getElementsByClassName("acrylic-head");
+    x[0].style.display = "block";
+}
